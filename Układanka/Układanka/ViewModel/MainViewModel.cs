eacf02104410feb36c44aeabca12a51516d1a2d4
rafcio96@gmail.com <rafcio96@gmail.com>
2018-08-11 23:53:00 +0200

    Add music for background

--- Układanka/Układanka/ViewModel/MainViewModel.cs
+++ Układanka/Układanka/ViewModel/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using Układanka.Helper;
 using System.ComponentModel;
+using System.Windows.Media;
 
 namespace Układanka.ViewModel
 {
@@ -14,11 +15,20 @@ namespace Układanka.ViewModel
     {
         private IMyNavigationService navigationService;
 
-
+        private MediaPlayer mediaPlayer;
+        private bool play = true;
         private string ChoosenSplit = null;
 
+        public RelayCommand SoundCommand { get; set; }
         public RelayCommand ChooseImageCommand { get; set; }
         public RelayCommand MixImageCommand { get; set; }
+
+        private string imageSound;
+        public string ImageSound
+        {
+            get { return imageSound; }
+            set { imageSound = value; RaisePropertyChanged(() => ImageSound); }
+        }
 
 
         private string image;
@@ -95,11 +105,27 @@ namespace Układanka.ViewModel
             }
         }
 
+        public void PlayOrStop()
+        {
+            if (play)
+            {
+                mediaPlayer.MediaEnded += new EventHandler(Media_Ended);
+                mediaPlayer.Play();
+            }
+            else
+                mediaPlayer.Pause();
+        }
+
         public MainViewModel(IMyNavigationService navService)
         {
             SelectedInd = -1;
             navigationService = navService;
             InitCommand();
+            ImageSound = "/Images/speaker.png";
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri("D:/c#/Nowy folder/Układanka/Układanka/Układanka/Music/Rick and Morty - Evil Morty.mp3"));
+            mediaPlayer.Volume = 150;
+            PlayOrStop();
         }
 
         public void InitCommand()
@@ -120,6 +146,16 @@ namespace Układanka.ViewModel
                     ChoosenSplit = null;
                     navigationService.NavigateTo(ViewModelLocator.DisplayImageKey,Image, true);
                 }
+            });
+
+            SoundCommand = new RelayCommand(() =>
+            {
+                if(play)
+                    ImageSound= "/Images/mute.png";
+                else
+                    ImageSound= "/Images/speaker.png";
+                play = !play;
+                PlayOrStop();
             });
 
             MixImageCommand = new RelayCommand(() =>
@@ -157,6 +193,12 @@ namespace Układanka.ViewModel
                     }
                 }
             });
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            mediaPlayer.Position = TimeSpan.Zero;
+            mediaPlayer.Play();
         }
     }
 }
